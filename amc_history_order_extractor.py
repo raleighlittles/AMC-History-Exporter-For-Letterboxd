e30309ac4b5e4044677e29b86d57196fad90f68b
Raleigh Littles III <raleighlittles@gmail.com>
2023-01-02 22:54:42 -0800

    more cleanup before publish
    fixed issue with movies in the future showing up in list, and better detection of cancelled events

--- amc_history_order_extractor.py
+++ amc_history_order_extractor.py
@@ -1,11 +1,19 @@
 import argparse
+import csv
+import datetime
 import json
 import pdb
-import csv
 
 if __name__ == "__main__":
+
+    argparse_parser = argparse.ArgumentParser()
+
+    argparse_parser.add_argument("-i", "--input-file", type=str, help="The text file containing order history. Must be copied directly from the 'order history', see documentation")
+    argparse_parser.add_argument("-o", "--output-file", type=str, help="The output CSV file. This is the file that will be imported into Letterboxd")
+
+    argparse_args = argparse_parser.parse_args()
     
-    order_history_filename = "order_history_2022.txt"
+    order_history_filename = argparse_args.input_file
     order_history_file = open(order_history_filename)
     order_history_str = order_history_file.read()
 
@@ -13,11 +21,16 @@ if __name__ == "__main__":
 
     print(len(order_history["edges"]), " orders found in file ", order_history_filename)
 
-    # Prepare the CSV file -- these have to match the letterboxd format
-    # https://letterboxd.com/about/importing-data/
-    csv_headers = ["Title", "Watched Date", "Ticket Cost", "Theater"]
+    # Prepare the CSV file -- see https://letterboxd.com/about/importing-data/
+    # We don't have a way of determining:
+    # (1) The year the movie was released, since AMC sometimes shows re-runs
+    # (2) The directors of the movie
+    # So the only column Letterboxd will actually use is the 'Title' column
+    csv_headers = ["Title", "Watched Date", "Theater", "Ticket Cost"]
 
-    csv_filename = "order_history.csv"
+    current_date = datetime.datetime.now()
+
+    csv_filename = argparse_args.output_file
 
     with open(csv_filename, 'w') as csv_file_handle:
 
@@ -26,19 +39,22 @@ if __name__ == "__main__":
 
         for order_event in order_history["edges"]:
 
-            # To account for the fact that the user can buy multiple tickets at once
             movies_from_order = list()
             showtimes = list()
             theaters = list()
             ticket_prices = list()
 
-            # Don't count orders that were refunded/cancelled
+            # Don't count orders that were not fulfilled
             if (order_event["node"]["status"] == "Fulfilled"):
 
                 for ticket_order in order_event["node"]["groups"]:
 
                     # Make sure the order isn't for a membership/concessions/etc
-                    if (ticket_order["type"].startswith("TICKET")):
+                    # and that no part of the order was cancelled/refunded
+                    if ((ticket_order["type"].startswith("TICKET")) and (ticket_order['cancelledCharges'] is None) and (ticket_order['refundedCharges'] is None)):
+
+                        # I'm not really sure why the order history is nested so much
+
                         movies_from_order.append(ticket_order["movie"]["name"])
                         showtimes.append(ticket_order["showtime"]["showDateTimeUtc"])
                         theaters.append(ticket_order["theatre"]["name"])
@@ -50,12 +66,21 @@ if __name__ == "__main__":
 
                         ticket_prices.append(tickets_cost)
 
+
                 if (len(movies_from_order) == len(showtimes) == len(theaters) == len(ticket_prices)):
+
                     for i in range(0, len(movies_from_order)):
                         # Desired date format is YYYY-MM-DD, and the timestamp looks like:
                         # 2021-12-18T20:00:00.000Z
-                        # so only extract the date part, ignoring the time
-                        csv_writer.writerow([movies_from_order[i], showtimes[i].split("T")[0], theaters[i], ticket_prices[i]])
+                        # We don't want to add entries for movies if the showing date
+                        # is in the future (the movie hasn't been seen yet)
+                        showtime_date = datetime.datetime.strptime(showtimes[i], "%Y-%m-%dT%H:%M:%S.%fZ")
+                        
+                        if (current_date > showtime_date):
+                            csv_writer.writerow([movies_from_order[i], showtimes[i].split("T")[0], theaters[i], ticket_prices[i]])
+
+                        else:
+                            print("Skipped ", movies_from_order[i], "since the showtime date was in the future")
 
                 else:
                     print("Unable to create row in CSV file, data doesn't match expected format")
